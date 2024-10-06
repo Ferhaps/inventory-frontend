188d1840d6ddb70f43d9ae4a1bbf7d342a99d4c1
Ferhan Cherkez <fericherkez14052002@gmail.com>
2024-10-06 18:09:57 +0300

    envs fixes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:7041/api',
+  backendUrl: 'https://localhost:7041/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://localhost:7041/api',
+  backendUrl: 'https://localhost:7041/api',
 };
