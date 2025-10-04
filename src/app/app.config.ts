640070572ee5791a1fe8513602e68edcf32a274a
Ferhan Cherkez <fericherkez14052002@gmail.com>
2025-10-04 13:34:11 +0300

    adds zoneless cd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { routes } from './routing/app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -7,6 +7,7 @@ import { hTTPInterceptor } from './services/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection(),
     provideRouter(routes, withComponentInputBinding(), withHashLocation()),
     provideHttpClient(withFetch(), withInterceptors([hTTPInterceptor])),
     provideAnimationsAsync(),
