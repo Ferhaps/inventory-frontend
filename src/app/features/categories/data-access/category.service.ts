5b0586c121c2327562e5bcef6d648c905eed0cfb
Ferhan Cherkez <fericherkez14052002@gmail.com>
2026-06-02 00:08:56 +0300

    refactors category fetching to use properties

--- src/app/features/categories/data-access/category.service.ts
+++ src/app/features/categories/data-access/category.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Category } from '../../../shared/types';
 import { JSON_HTTP_OPTIONS } from '../../../shared/utils';
 
@@ -13,8 +13,8 @@ export class CategoryService {
 
 	private http = inject(HttpClient);
 
-	public getCategories(): Observable<Category[]> {
-		return this.http.get<Category[]>(this.categoryUrl, JSON_HTTP_OPTIONS);
+	public getCategories(): Promise<Category[]> {
+		return firstValueFrom(this.http.get<Category[]>(this.categoryUrl, JSON_HTTP_OPTIONS));
 	}
 
 	public addCategory(name: string): Observable<Category> {
--- src/app/features/categories/store/categories.store.ts
+++ src/app/features/categories/store/categories.store.ts
@@ -1,9 +1,7 @@
-import { computed, inject } from "@angular/core";
+import { inject } from "@angular/core";
 import { Category } from "../../../shared/types";
-import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals";
+import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
 import { CategoryService } from "../data-access/category.service";
-import { rxMethod } from "@ngrx/signals/rxjs-interop";
-import { filter, pipe, switchMap, tap } from "rxjs";
 
 type CategoriesStatus = 'idle' | 'loading' | 'loaded' | 'error';
 
@@ -20,20 +18,17 @@ export const CategoriesStore = signalStore(
 	}),
 
 	withMethods((store, categoryService = inject(CategoryService)) => ({
-		load: rxMethod<void>(
-			pipe(
-				filter(() => store.status() === 'idle'),
-				tap(() => patchState(store, { status: 'loading' })),
-				switchMap(() =>
-					categoryService.getCategories().pipe(
-						tap({
-							next: (categories: Category[]) => patchState(store, { categories, status: 'loaded' }),
-							error: () => patchState(store, { status: 'error' })
-						})
-					)
-				)
-			)
-		),
+		async load(): Promise<void> {
+			if (store.status() !== 'idle') return;
+			patchState(store, { status: 'loading' });
+
+			try {
+				const categories = await categoryService.getCategories();
+				patchState(store, { categories, status: 'loaded' });
+			} catch {
+				patchState(store, { status: 'error' });
+			}
+	},
 
 		addOne(category: Category): void {
 			patchState(store, { categories: [...store.categories(), category] });
